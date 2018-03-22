c2680a21c21c9eafdd25861f703478c4c59488c7
nest ped <nestza555@gmail.com>
2018-03-23 01:24:43 +0700

    finish tx-api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,134 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const express = require("express");
+const axios = require("axios");
+const admin = require("firebase-admin");
+const Querystring = require("query-string");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require("./service-account");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://six-dashboard.firebaseio.com"
+});
+
+const fireStore = admin.firestore();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const BASE_URL = "https://sixnetwork.api-us1.com";
+const API_KEY =
+  "3a2ccae1fc73e46759aa88291b3b7179282f77acf2e87fb77a26237a18b7335082a0b2d2";
+
+function getUser(uid) {
+  return new Promise((resolve, reject) => {
+    fireStore
+      .collection("users")
+      .doc(uid)
+      .get()
+      .then(snapshot => snapshot.data())
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+function updateUser(uid, data) {
+  return new Promise((resolve, reject) => {
+    fireStore
+      .collection("users")
+      .doc(uid)
+      .set(data, { merge: true })
+      .then(snapshot => snapshot.data())
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+function insertUserTx(uid, data) {
+  return getUser(uid)
+    .then(result => {
+      const { xlm_tx } = result;
+      let txs = xlm_tx ? xlm_tx : [];
+      txs = txs.concat([data]);
+      return updateUser(uid, { xlm_tx: txs });
+    })
+    .catch(err => err);
+}
+
+app.use("/users/:uid", (req, res) => {
+  const { uid } = req.params;
+  getUser(uid)
+    .then(result => res.send(result))
+    .catch(err => res.status(400).send(err));
+});
+
+app.use("/purchase-list", (req, res) => {
+  return fireStore
+    .collection("order-histories")
+    .get()
+    .then(querySnapshot => {
+      let result = [];
+      querySnapshot.forEach(function(doc) {
+        const data = doc.data();
+        result = result.concat([data]);
+      });
+      return result;
+    })
+    .then(result => res.send(result))
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+app.post("/purchase/:currency", (req, res) => {
+  const { currency } = req.params;
+  const { buyer_id, amount, time, total_six, id } = req.body;
+  const data = {
+    buyer_id,
+    id,
+    amount,
+    time,
+    total_six,
+    currency
+  };
+  return fireStore
+    .collection("order-histories")
+    .doc(id)
+    .set(Object.assign(data, { status: "success" }))
+    .then(function(result) {
+      insertUserTx(buyer_id, data);
+      res.send(result);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
+
+exports.sendCampaignEmail = functions.database
+  .ref("/users/{uid}")
+  .onCreate(event => {
+    const snapshot = event.data;
+    const data = snapshot.val();
+    const { email, firstName, lastName, phone } = data;
+    const queryString = `email=${email}&first_name=${firstName}&last_name=${lastName}&phone=${phone}`;
+    const postObj = Querystring.stringify({
+      email: email,
+      fist_name: firstName,
+      last_name: lastName,
+      phone: phone
+    });
+    const url = `${BASE_URL}/admin/api.php?api_action=contact_add&api_key=${API_KEY}&api_output=json`;
+    return axios
+      .post(url, postObj)
+      .then(res => res.data)
+      .then(data => {
+        console.log(data, "res from activecampaign");
+      })
+      .catch(err => console.log(err, "error send email"));
+  });
