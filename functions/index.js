a7fe6ff102372f6670973b2a4e0b765975d5c073
Methuz Kaewsaikao <methuz.k@gmail.com>
2018-03-25 02:31:55 +0700

    Add hourly functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const axios = require("axios");
-const admin = require("firebase-admin");
 const Querystring = require("query-string");
+const admin = require("firebase-admin");
+const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const express = require("express");
+const functions = require("firebase-functions");
+const request = require('request-promise');
+const moment = require('moment-timezone');
 
 const serviceAccount = require("./service-account");
 
@@ -17,12 +19,15 @@ const fireStore = admin.firestore();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 const BASE_URL = "https://sixnetwork.api-us1.com";
-const API_KEY =
-  "3a2ccae1fc73e46759aa88291b3b7179282f77acf2e87fb77a26237a18b7335082a0b2d2";
+const API_KEY = "3a2ccae1fc73e46759aa88291b3b7179282f77acf2e87fb77a26237a18b7335082a0b2d2";
+
+const getBasePriceURI = (coin) => `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`
 
 function getUser(uid) {
   return new Promise((resolve, reject) => {
@@ -41,7 +46,9 @@ function updateUser(uid, data) {
     fireStore
       .collection("users")
       .doc(uid)
-      .set(data, { merge: true })
+      .set(data, {
+        merge: true
+      })
       .then(snapshot => snapshot.data())
       .then(result => resolve(result))
       .catch(err => reject(err));
@@ -51,16 +58,18 @@ function updateUser(uid, data) {
 function insertUserTx(uid, data) {
   return getUser(uid)
     .then(result => {
-      const { xlm_tx } = result;
+      const {xlm_tx} = result;
       let txs = xlm_tx ? xlm_tx : [];
       txs = txs.concat([data]);
-      return updateUser(uid, { xlm_tx: txs });
+      return updateUser(uid, {
+        xlm_tx: txs
+      });
     })
     .catch(err => err);
 }
 
 app.use("/users/:uid", (req, res) => {
-  const { uid } = req.params;
+  const {uid} = req.params;
   getUser(uid)
     .then(result => res.send(result))
     .catch(err => res.status(400).send(err));
@@ -85,8 +94,8 @@ app.use("/purchase-list", (req, res) => {
 });
 
 app.post("/purchase/:currency", (req, res) => {
-  const { currency } = req.params;
-  const { buyer_id, amount, time, total_six, id } = req.body;
+  const {currency} = req.params;
+  const {buyer_id, amount, time, total_six, id} = req.body;
   const data = {
     buyer_id,
     id,
@@ -98,10 +107,14 @@ app.post("/purchase/:currency", (req, res) => {
   return fireStore
     .collection("order-histories")
     .doc(id)
-    .set(Object.assign(data, { status: "success" }))
+    .set(Object.assign(data, {
+      status: "success"
+    }))
     .then(function(result) {
       insertUserTx(buyer_id, data);
-      res.send(Object.assign(data, { status: "success" }));
+      res.send(Object.assign(data, {
+        status: "success"
+      }));
     })
     .catch(err => {
       res.status(400).send(err);
@@ -115,7 +128,7 @@ exports.sendCampaignEmail = functions.database
   .onCreate(event => {
     const snapshot = event.data;
     const data = snapshot.val();
-    const { email, firstName, lastName, phone } = data;
+    const {email, firstName, lastName, phone} = data;
     const queryString = `email=${email}&first_name=${firstName}&last_name=${lastName}&phone=${phone}`;
     const postObj = Querystring.stringify({
       email: email,
@@ -132,3 +145,78 @@ exports.sendCampaignEmail = functions.database
       })
       .catch(err => console.log(err, "error send email"));
   });
+
+
+function getTime() {
+  const time = new Date()
+
+  if (time.getMinutes > 55) {
+    time.setHours(time.getHours() + 1)
+  }
+
+  time.setMinutes(0, 0, 0)
+
+  const timeString = moment.tz(time, 'Asia/Bangkok').toString()
+
+  return {
+    unix: time.getTime(),
+    string: timeString
+  }
+}
+
+exports.hourly_xlm = functions.pubsub.topic('hourly-xlm').onPublish((event) => {
+  const baseToken = 'xlm'
+  return handleHourlyEvent(event, baseToken)
+});
+
+exports.hourly_eth = functions.pubsub.topic('hourly-eth').onPublish((event) => {
+  const baseToken = 'eth'
+  return handleHourlyEvent(event, baseToken)
+});
+
+exports.hourly_btc = functions.pubsub.topic('hourly-btc').onPublish((event) => {
+  const baseToken = 'btc'
+  return handleHourlyEvent(event, baseToken)
+});
+
+function handleHourlyEvent(event, baseToken) {
+  const time = getTime()
+  const uri = getBasePriceURI(baseToken.toUpperCase())
+
+  return request({
+    uri,
+    method: 'GET',
+    json: true
+  })
+    .then((body) => {
+      return updateHourlyPrice(body, baseToken, time)
+    })
+}
+
+function updateHourlyPrice(body, baseToken, time) {
+  const price = body.USD / 0.1
+  return fireStore
+    .collection(`${baseToken}-prices`)
+    .doc(time.unix.toString())
+    .set({
+      time: time.unix,
+      price: body.USD,
+      [`six_per_${baseToken}`]: price,
+      time_string: time.string
+    })
+}
+
+exports.getUniqueId = functions.https.onRequest((req, res) => {
+  var ref = admin.database().ref('/users/lastUserId');
+  ref.transaction(function(current) {
+    return (current || 0) + 1;
+  }, function(error, committed, snapshot) {
+    if (error || !committed || !snapshot) {
+      console.error("Transaction failed abnormally!", error || "");
+      res.status(400).send(err);
+    } else {
+      console.log("Generated ID: ", snapshot.val());
+    }
+    res.status(200).send(snapshot.val().toString());
+  });
+});
