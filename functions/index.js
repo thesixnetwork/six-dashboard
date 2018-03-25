ff2a82eecaa1f21e6321a755193af6058c861cc4
Watcharapon <watcharapon@Watcharapons-MacBook-Pro.local>
2018-03-25 13:12:28 +0700

    standard js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,5 @@
-/*jshint esversion: 6 */
 const functions = require('firebase-functions')
 const axios = require('axios')
-const admin = require('firebase-admin')
 const Querystring = require('query-string')
 const API_KEY = functions.config().campaign.api_key
 const BASE_URL = functions.config().campaign.base_url
@@ -13,24 +11,23 @@ const BASE_URL = functions.config().campaign.base_url
 //  response.send("Hello from Firebase!");
 // });
 
-exports.sendCampaignEmail = functions.firestore.document("/users/{uid}").onCreate(event => {
-  const snapshot = event.data;
-  const data = snapshot.data();
-  const { email, firstName, lastName, phone } = data;
-  const queryString = `email=${email}&first_name=${firstName}&last_name=${lastName}&phone=${phone}`;
+exports.sendCampaignEmail = functions.firestore.document('/users/{uid}').onCreate(event => {
+  const snapshot = event.data
+  const data = snapshot.data()
+  const { email, firstName, lastName, phone } = data
   const postObj = Querystring.stringify({
-    "email": email,
-    "fist_name": firstName,
-    "last_name": lastName,
-    "phone": phone
-  });
-  const url = `${BASE_URL}/admin/api.php?api_action=contact_add&api_key=${API_KEY}&api_output=json`;
+    'email': email,
+    'fist_name': firstName,
+    'last_name': lastName,
+    'phone': phone
+  })
+  const url = `${BASE_URL}/admin/api.php?api_action=contact_add&api_key=${API_KEY}&api_output=json`
   return axios.post(url, postObj)
     .then(res => res.data)
     .then(data => {
-      return console.log(data, 'res from activecampaign');
+      return console.log(data, 'res from activecampaign')
     })
     .catch(err => {
-      return console.log(err, 'error send email');
-    });
-});
+      return console.log(err, 'error send email')
+    })
+})
