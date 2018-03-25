4cbffa13f29b0ed49d93eb7f42a4cd792f5e123a
Methuz Kaewsaikao <methuz.k@gmail.com>
2018-03-26 01:47:27 +0700

    Handle undefined_tx

--- functions/stellar-service.js
+++ functions/stellar-service.js
@@ -37,65 +37,76 @@ function StellarService(event) {
         break;
       }
 
+
       payments.records[i].transaction().then(function(tx) {
-        fireStore
+        if (!tx.hasOwnProperty('memo')) {
+          return {
+              tx
+          }
+        }
+        return fireStore
           .collection('users')
           .where('xlm_memo', '==', tx.memo)
           .get()
           .then((snapshot) => {
             const users = [];
             snapshot.forEach((user) => {
-                let _user = user.data()
-                _user.id = user.id
-                users.push(_user)
+              let _user = user.data()
+              _user.id = user.id
+              users.push(_user)
             })
-            return users[0]
+            return {
+                tx,
+                user: users[0]
+            }
           })
-          .then((user) => {
-            tx.operations().then((operations) => {
-              const records = operations._embedded.records
-              const operationLength = operations._embedded.records.length
-              for (let j = 0; j < operationLength; j++) {
-                if (records[j].type !== 'payment') {
-                  continue
+      }).then((body) => {
+        const tx = body.tx
+        const user = body.user
+
+        tx.operations().then((operations) => {
+          const records = operations._embedded.records
+          const operationLength = operations._embedded.records.length
+          for (let j = 0; j < operationLength; j++) {
+            if (records[j].type !== 'payment') {
+              continue
+            }
+            if (records[j].to !== this.address) {
+              continue
+            }
+
+            const time = new Date()
+            const timeZero = time.setMinutes(0, 0, 0)
+
+            fireStore
+              .collection('xlm_prices')
+              .doc(timeZero.toString())
+              .get()
+              .then(snapshot => snapshot.data())
+              .then((price) => {
+                if (!price.price) {
+                  time.setHours(time.getHours() - 1, 0, 0, 0)
+                  fireStore
+                    .collection('xlm_prices')
+                    .doc(timeZero.toString())
+                    .get()
+                    .then(snapshot => snapshot.data())
+                    .then((price) => {
+                      return handleOperation(tx, records[j], j, price, timeZero)
+                    })
                 }
-                if (records[j].to !== this.address) {
-                  continue
-                }
 
-                const time = new Date()
-                const timeZero = time.setMinutes(0, 0, 0)
+                return handleOperation(user, tx, records[j], j, price, timeZero)
+              }).then((update) => {
 
-                fireStore
-                  .collection('xlm_prices')
-                  .doc(timeZero.toString())
-                  .get()
-                  .then(snapshot => snapshot.data())
-                  .then((price) => {
-                    if (!price.price) {
-                      time.setHours(time.getHours() - 1, 0, 0, 0)
-                      fireStore
-                        .collection('xlm_prices')
-                        .doc(timeZero.toString())
-                        .get()
-                        .then(snapshot => snapshot.data())
-                        .then((price) => {
-                          return handleOperation(tx, records[j], j, price, timeZero)
-                        })
-                    }
-
-                    return handleOperation(user, tx, records[j], j, price, timeZero)
-                  }).then((update) => {
-
-                  if (new Date(payments.records[i].created_at).getTime() <= cursor) {
-                    return update
-                  }
-
-                  return updateCursor(payments, i)
-                })
+              if (new Date(payments.records[i].created_at).getTime() <= cursor) {
+                return update
               }
+
+              return updateCursor(payments, i)
             })
-          })
+          }
+        })
       })
     }
   })
@@ -113,8 +124,7 @@ function updateCursor(payments, i) {
 function handleOperation(user, tx, operation, n, price, priceTime) {
   const hash = tx.hash
   const id = `${hash}_${n}`
-  const user_id = user.id
-  const memo = tx.memo
+  const memo = tx.memo || ''
   const native_amount = +operation.amount
   const six_amount = +(operation.amount * price.six_per_xlm).toFixed(7)
   const receive_account = tx.source_account
@@ -130,7 +140,6 @@ function handleOperation(user, tx, operation, n, price, priceTime) {
 
   const body = {
     id,
-    user_id,
     time,
     price_time,
     total_usd_price,
@@ -148,6 +157,9 @@ function handleOperation(user, tx, operation, n, price, priceTime) {
       .doc(`${hash}_${operation.id}`)
       .set(body)
   }
+
+  const user_id = user.id
+  body.user_id = user_id
 
   return fireStore
     .collection('purchase_txs')
