804960d2719248232a497df3992c935c84f3547f
Prach Hormanee <mar@Prachs-MacBook-Pro.local>
2018-03-26 04:39:03 +0700

    add cloud function `incrementTotalAsset` for trigger incoming new tx

--- functions/index.js
+++ functions/index.js
@@ -123,6 +123,23 @@ app.post('/purchase/:currency', (req, res) => {
 
 exports.api = functions.https.onRequest(app)
 
+exports.incrementTotalAsset = functions.firestore.document('/purchase_txs/{txId}')
+  .onCreate(event => {
+    const data = event.data.data()
+    console.log('Create Transaction:', event.params.txId, data.type, data.native_amount)
+    const assetCol = fireStore.collection('total_asset')
+    return fireStore.runTransaction(tx => Promise.all([
+      {type: data.type, key: 'native_amount'},
+      {type: 'usd', key: 'total_usd_price'},
+      {type: 'six', key: 'six_amount'}
+    ].map(asset => {
+      const ref = assetCol.doc(asset.type)
+      return tx.get(ref).then(assetDoc => tx.update(ref, {total: assetDoc.data().total + data[asset.key]}))
+    })
+    )
+    )
+  })
+
 exports.sendCampaignEmail = functions.database
   .ref('/users/{uid}')
   .onCreate(event => {
