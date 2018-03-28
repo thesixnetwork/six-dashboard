98926752fbdc4c8f3532395f248afe6024403797
Prach Hormanee <mar@Prachs-MacBook-Pro.local>
2018-03-28 14:58:23 +0700

    remove duplicate function

--- functions/index.js
+++ functions/index.js
@@ -44,23 +44,6 @@ function updateUser (uid, data) {
   })
 }
 
-exports.incrementTotalAsset = functions.firestore.document('/purchase_txs/{txId}')
-  .onCreate(event => {
-    const data = event.data.data()
-    console.log('Create Transaction:', event.params.txId, data.type, data.native_amount)
-    const assetCol = fireStore.collection('total_asset')
-    return fireStore.runTransaction(tx => Promise.all([
-      {type: data.type, key: 'native_amount'},
-      {type: 'usd', key: 'total_usd_price'},
-      {type: 'six', key: 'six_amount'}
-    ].map(asset => {
-      const ref = assetCol.doc(asset.type)
-      return tx.get(ref).then(assetDoc => tx.update(ref, {total: assetDoc.data().total + data[asset.key]}))
-    })
-    )
-    )
-  })
-
 function generatePhoneVerificationCode (phone_number) {
   let refCode = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5).toUpperCase()
   let code = Math.random().toString().substr(2, 6)
