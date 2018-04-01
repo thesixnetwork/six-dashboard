f9e16d8eb4c84133fa091d9bc672aac4f9a73e9b
Watcharapon <watcharapon@Watcharapons-MacBook-Pro.local>
2018-04-01 19:04:27 +0700

    fix #67 aditlog version 1

--- functions/trigger/index.js
+++ functions/trigger/index.js
@@ -1,6 +1,7 @@
 module.exports = function (functions, fireStore) {
   return [
     ...require('./purchaseTransaction')(functions, fireStore),
-    ...require('./user')(functions, fireStore)
+    ...require('./user')(functions, fireStore),
+    ...require('./auditlog')(functions, fireStore)
   ]
 }
