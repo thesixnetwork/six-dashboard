4c312f9089460ae23a0ef1b5386e7ef9f2528580
Gas <fallen_things@hotmail.com>
2018-05-07 13:24:04 +0700

    change to users_claim

--- functions/claim-service.js
+++ functions/claim-service.js
@@ -2,9 +2,10 @@ const functions = require('firebase-functions')
 const StellarSdk = require('stellar-sdk')
 const admin = require('firebase-admin')
 const db = admin.firestore()
+const claimRef = db.collection('users_claim')
 
 let stellarUrl
-if (functions.config().campaign.is_production === "true") {
+if (functions.config().campaign.is_production === 'true') {
   stellarUrl = 'https://horizon.stellar.org'
   StellarSdk.Network.usePublicNetwork()
 } else {
@@ -14,8 +15,12 @@ if (functions.config().campaign.is_production === "true") {
 
 const server = new StellarSdk.Server(stellarUrl)
 
-const issuerKey = StellarSdk.Keypair.fromSecret(functions.config().xlm.issuer_low_secret)
-const distKey = StellarSdk.Keypair.fromSecret(functions.config().xlm.ico_distributor_secret)
+const issuerKey = StellarSdk.Keypair.fromSecret(
+  functions.config().xlm.issuer_low_secret
+)
+const distKey = StellarSdk.Keypair.fromSecret(
+  functions.config().xlm.ico_distributor_secret
+)
 
 const sixAsset = new StellarSdk.Asset('six', issuerKey.publicKey())
 
@@ -47,34 +52,40 @@ const handleCreateStellarAccount = (req, res) => {
       return res.status(200).json({
         done: true
       })
-    }).catch((error) => {
-    console.dir(error)
-    return res.status(503).json({
-      error
     })
-  })
-
+    .catch(error => {
+      console.dir(error)
+      return res.status(503).json({
+        error
+      })
+    })
 }
 
-const setPublicKey = ({uid, public_key: publicKey}) => {
-  return db.collection('users').doc(uid).update({
-    "claim.public_key": publicKey
-  }).then(() => {
-    return {
-      uid,
-      public_key: publicKey
-    }
-  })
+const setPublicKey = ({ uid, public_key: publicKey }) => {
+  // @TODO check is users exists?
+  // @TODO check is users_claim exists?
+  return claimRef
+    .doc(uid)
+    .update({
+      'claim.public_key': publicKey
+    })
+    .then(() => {
+      return {
+        uid,
+        public_key: publicKey
+      }
+    })
 }
 
-const createStellarAccount = ({uid, public_key: publicKey}) => {
-
-  function createTransaction(distributorAccount) {
+const createStellarAccount = ({ uid, public_key: publicKey }) => {
+  function createTransaction (distributorAccount) {
     const transaction = new StellarSdk.TransactionBuilder(distributorAccount)
-      .addOperation(StellarSdk.Operation.createAccount({
-        destination: publicKey,
-        startingBalance
-      }))
+      .addOperation(
+        StellarSdk.Operation.createAccount({
+          destination: publicKey,
+          startingBalance
+        })
+      )
       .build()
 
     transaction.sign(distKey)
@@ -85,7 +96,7 @@ const createStellarAccount = ({uid, public_key: publicKey}) => {
     }
   }
 
-  function submitTransaction({uid, public_key: publicKey, transaction}) {
+  function submitTransaction ({ uid, public_key: publicKey, transaction }) {
     return server.submitTransaction(transaction).then(() => {
       return {
         uid,
@@ -94,15 +105,18 @@ const createStellarAccount = ({uid, public_key: publicKey}) => {
     })
   }
 
-  return server.loadAccount(distKey.publicKey())
+  return server
+    .loadAccount(distKey.publicKey())
     .then(createTransaction)
     .then(submitTransaction)
 }
 
-const updateUserCreatedAccount = ({uid}) => {
-  return db.collection('users').doc(uid).update({
-    "claim.sent_xlm": true
-  })
+const updateUserCreatedAccount = ({ uid }) => {
+  return claimRef
+    .doc(uid)
+    .update({
+      'claim.sent_xlm': true
+    })
 }
 
 const handleClaimSix = (req, res) => {
@@ -134,36 +148,38 @@ const handleClaimSix = (req, res) => {
       return res.status(200).json({
         done: true
       })
-    }).catch((error) => {
-    console.dir(error)
-    return res.status(503).json({
-      error
     })
-  })
+    .catch(error => {
+      console.dir(error)
+      return res.status(503).json({
+        'error': error.message
+      })
+    })
 }
 
-function findUser({uid, claim_id}) {
-  return db.collection('users').doc(uid).get().then((user) => {
-    if (user.exists) {
-      return {
-        uid,
-        claim_id,
-        user: user.data()
+function findUser ({ uid, claim_id: claimId }) {
+  return claimRef
+    .doc(uid)
+    .get()
+    .then(user => {
+      if (user.exists) {
+        return {
+          uid,
+          claim_id: claimId,
+          user: user.data()
+        }
       }
-    }
-    return Promise.reject({
-      'text': 'User not found'
+      return Promise.reject(new Error('User not found'))
     })
-  })
 }
 
-function findClaim({uid, claim_id: claimId, user}) {
-  return db.collection('users')
+function findClaim ({ uid, claim_id: claimId, user }) {
+  return claimRef
     .doc(uid)
     .collection('claim_period')
     .doc(claimId)
     .get()
-    .then((claim) => {
+    .then(claim => {
       if (claim.exists) {
         return {
           uid,
@@ -172,108 +188,128 @@ function findClaim({uid, claim_id: claimId, user}) {
           user
         }
       }
-      return Promise.reject({
-        'text': 'User not found'
-      })
+      return Promise.reject(new Error('User not found'))
     })
 }
 
-function allowTrust({uid, claim_id, user, claim}) {
-  function createTransaction(issuerAccount) {
+function allowTrust ({ uid, claim_id: claimId, user, claim }) {
+  function createTransaction (issuerAccount) {
     const transaction = new StellarSdk.TransactionBuilder(issuerAccount)
-      .addOperation(StellarSdk.Operation.allowTrust({
-        trustor: user.claim.public_key,
-        assetCode: 'six',
-        authorize: true
-      }))
+      .addOperation(
+        StellarSdk.Operation.allowTrust({
+          trustor: user.public_key,
+          assetCode: 'six',
+          authorize: true
+        })
+      )
       .build()
 
     transaction.sign(issuerKey)
     return {
       uid,
       claim,
-      claim_id,
+      claim_id: claimId,
       user,
       transaction
     }
   }
 
-  function submitTransaction({uid, public_key: publicKey, transaction}) {
+  function submitTransaction ({ uid, public_key: publicKey, transaction }) {
     return server.submitTransaction(transaction).then(() => {
       return {
         uid,
         claim,
-        claim_id,
-        user,
+        claimId,
+        user
       }
     })
   }
 
-  return server.loadAccount(issuerKey.publicKey())
+  return server
+    .loadAccount(issuerKey.publicKey())
     .then(createTransaction)
     .then(submitTransaction)
 }
 
-const updateAllowTrust = ({uid, claim, claim_id, user}) => {
-  return db.collection('users').doc(uid).update({
-    "claim.allow_trust": true
-  }).then(() => {
-    return {
-      uid,
-      claim,
-      claim_id,
-      user,
-    }
-  })
+const updateAllowTrust = ({ uid, claim, claim_id: claimId, user }) => {
+  return claimRef
+    .doc(uid)
+    .update({
+      'claim.allow_trust': true
+    })
+    .then(() => {
+      return {
+        uid,
+        claim,
+        claim_id: claimId,
+        user
+      }
+    })
 }
 
-function sendSix({uid, claim_id, user, claim}) {
-  function createTransaction(distributorAccount) {
-    const sendTransaction = new StellarSdk.TransactionBuilder(distributorAccount)
-      .addOperation(StellarSdk.Operation.payment({
-        destination: user.claim.public_key,
-        amount: claim.amount.toString(),
-        asset: sixAsset
-      }))
+function sendSix ({ uid, claim_id: claimId, user, claim }) {
+  function createTransaction (distributorAccount) {
+    const sendTransaction = new StellarSdk.TransactionBuilder(
+      distributorAccount
+    )
+      .addOperation(
+        StellarSdk.Operation.payment({
+          destination: user.public_key,
+          amount: claim.amount.toString(),
+          asset: sixAsset
+        })
+      )
       .build()
 
     sendTransaction.sign(distKey)
     return {
       uid,
       claim,
-      claim_id,
+      claim_id: claimId,
       user,
       send_transaction: sendTransaction
     }
   }
 
-  function submitTransaction({uid, claim, claim_id, user, send_transaction:sendTransaction}) {
+  function submitTransaction ({
+    uid,
+    claim,
+    claim_id: claimId,
+    user,
+    send_transaction: sendTransaction
+  }) {
     return server.submitTransaction(sendTransaction).then(() => {
       return {
         uid,
         claim,
-        claim_id,
-        user,
+        claim_id: claimId,
+        user
       }
     })
   }
 
-  return server.loadAccount(distKey.publicKey())
+  return server
+    .loadAccount(distKey.publicKey())
     .then(createTransaction)
     .then(submitTransaction)
 }
 
-const updateClaim = ({uid, claim, claim_id, user}) => {
-  return db.collection('users').doc(uid).collection('claim_period').doc(claim_id).update({
-    "claimed": true
-  }).then(() => {
-    return {
-      uid,
-      claim,
-      claim_id,
-      user,
-    }
-  })
+const updateClaim = ({ uid, claim, claim_id: claimId, user }) => {
+  return claimRef
+    .doc(uid)
+    .collection('claim_period')
+    .doc(claimId)
+    .update({
+      claimed: true
+    })
+    .then(() => {
+      return {
+        uid,
+        claim,
+        claim_id: claimId,
+        user
+      }
+    })
 }
 
 module.exports = {
