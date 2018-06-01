a6fe2f7aea429e324a4c6034ad221db65b6d67fb
Theerawit Akeairipong <>
2018-06-01 14:10:57 +0700

    add account_creator_secret to deploy_config

--- deploy_config/development.sh
+++ deploy_config/development.sh
@@ -2,3 +2,4 @@ firebase functions:config:set campaign.api_key="3a2ccae1fc73e46759aa88291b3b7179
 firebase functions:config:set email.user="cglove005@gmail.com" email.password="sixnetwork" email.host="smtp.gmail.com" email.port="465" email.from='"Six network ICO." <noreply@firebase.com>'
 firebase functions:config:set xlm.address="GDJGQJHY3FNOP7P3BBSUHDKP5NMZL7FABPCBFE7W3OTDVEHEQMVX6B32" eth.address="0x51ac9a6bd8de95898a85fa6dbd5f1bfef957d854" xlm.issuer_public="GBVX36SLDLLXCVMGFLKNQ5XB76Z4SIXCFKYHKMSJTLANXB6AH27LUKEP" xlm.ico_distributor_secret="SAWBJWTWQAOPBGSEQQFVJYXXDM4JVRH2NOKOJ47BQCVGT2RNALGPV5MK"
 firebase functions:config:set secondary_signer.url="https://us-central1-multisigner-21cd5.cloudfunctions.net"
+firebase functions:config:set xlm.account_creator_secret="SCNV255Q7E6GEBXZJRESDV6BYQ6PDJMUCJI7SQPCU52PCYLLGRDXPW4T"
