2b1123e34b2522f85ab3bfb04f531f0a14abfea6
Prach Hormanee <>
2018-06-06 19:04:50 +0700

    add missing trigger file

--- functions/trigger/index.js
+++ functions/trigger/index.js
@@ -2,6 +2,7 @@ module.exports = function (admin, functions, fireStore) {
   return [
     ...require('./purchaseTransaction')(functions, fireStore),
     ...require('./user')(admin, functions, fireStore),
-    ...require('./auditlog')(functions, fireStore)
+    ...require('./auditlog')(functions, fireStore),
+    ...require('./claimProcess')(functions, fireStore)
   ]
 }
