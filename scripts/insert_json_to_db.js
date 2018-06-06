543a988ca28002f2176b3bd03461371f3611e054
Prach Hormanee <>
2018-06-06 10:55:43 +0700

    remove redundant file

--- scripts/insert_json_to_db.js
+++ scripts/insert_json_to_db.js
@@ -12,19 +12,19 @@ const airdropUsers = require(airdropUserPath)
 const serviceAccount = require(configPath)
 const allUsers = [...privateUsers, ...publicUsers, ...airdropUsers]
 
-function merge(allUsers) {
-    const groupByUID = R.groupBy(user => user.uid)
+function merge (allUsers) {
+  const groupByUID = R.groupBy(user => user.uid)
 
-    const concatValues = (k, l, r) => k == 'claim_periods' ? R.concat(l, r) : r
+  const concatValues = (k, l, r) => k == 'claim_periods' ? R.concat(l, r) : r
 
-    const reducePeriods = R.reduce(R.mergeWithKey(concatValues), {})
+  const reducePeriods = R.reduce(R.mergeWithKey(concatValues), {})
 
-    return R.pipe(groupByUID, R.mapObjIndexed(reducePeriods), R.values)(allUsers)
+  return R.pipe(groupByUID, R.mapObjIndexed(reducePeriods), R.values)(allUsers)
 }
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://six-dashboard.firebaseio.com'
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 })
 
 const db = admin.firestore()
@@ -39,24 +39,24 @@ try {
  * main
  */
 
-async function main() {
+async function main () {
   const mergeUsers = merge(allUsers)
   await updateUsers(mergeUsers)
   console.log('done')
   process.exit(0)
 }
 
-async function updateUsers(privateUsers) {
+async function updateUsers (privateUsers) {
   return await Promise.all(privateUsers.map(async (privateUser) => {
-      return updateUser(privateUser)
-    }))
+    return updateUser(privateUser)
+  }))
 }
 
 /**
  * update each user
  * @return Promise()
  */
-async function updateUser(privateUser) {
+async function updateUser (privateUser) {
   const email = privateUser.email
   let uid = privateUser.uid || null
 
@@ -73,15 +73,15 @@ async function updateUser(privateUser) {
   if (userData === null) {
     await insertUser(uid)
     return await Promise.all(claimData.map(async (claimDatum, i) => {
-        return await insertClaimPeriods(i.toString(), uid, claimDatum)
-      }))
+      return await insertClaimPeriods(i.toString(), uid, claimDatum)
+    }))
   } else if (userData !== null &&
     userPeriods.length === 0 &&
     claimData.length > 0
   ) {
     return await Promise.all(claimData.map(async (claimDatum, i) => {
-        return await insertClaimPeriods(i.toString(), uid, claimDatum)
-      }))
+      return await insertClaimPeriods(i.toString(), uid, claimDatum)
+    }))
   } else {
     // find extra clai from userPeriods then insert
     const mapIndexed = R.addIndex(R.map)
@@ -99,15 +99,15 @@ async function updateUser(privateUser) {
     )
     const extra = filter(claimData)
     return await Promise.all(extra.map(async ({i, claim}) => {
-        return await insertClaimPeriods(i.toString(), uid, claim)
-      }))
+      return await insertClaimPeriods(i.toString(), uid, claim)
+    }))
   }
 }
 
 /**
  * @return user
  */
-async function getUserByEmail(email) {
+async function getUserByEmail (email) {
   return admin
     .auth()
     .getUserByEmail(email)
@@ -116,7 +116,7 @@ async function getUserByEmail(email) {
 /**
  * @return userData
  */
-async function getUserFromDB(uid) {
+async function getUserFromDB (uid) {
   // db.users_claim.uid
   const docRef = db
     .collection('users_claim')
@@ -144,8 +144,8 @@ async function getUserFromDB(uid) {
         return [doc.data(), periods]
       }
     }).catch((error) => {
-    throw error
-  })
+      throw error
+    })
   return {
     user,
     periods
@@ -155,7 +155,7 @@ async function getUserFromDB(uid) {
 /**
  * insert user to uid in users_clim collection
  */
-async function insertUser(uid) {
+async function insertUser (uid) {
   // insert user
   const insertUserData = {
     public_key: '',
@@ -172,7 +172,7 @@ async function insertUser(uid) {
 /**
  * insert claim period
  */
-async function insertClaimPeriods(periodId, uid, claimData) {
+async function insertClaimPeriods (periodId, uid, claimData) {
   return db
     .collection('users_claim')
     .doc(uid)
